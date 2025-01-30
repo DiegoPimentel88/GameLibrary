5f83acfa61d3c820535ba32a21ad9f2ee7536652
Diego Pimentel Montoro Leite <diego.pimentel88@gmail.com>
2025-01-29 22:27:59 -0300

    Adicionando mais elementos e Trocando alguns displays enquanto aprendo ainda mais

--- index/mainfiles/index.js
+++ index/mainfiles/index.js
@@ -1,4 +1,4 @@
-const user = "Kauan"
+const user = "Carloman"
 
 const userInScreen = document.getElementById("user");
 
@@ -6,4 +6,3 @@ function usuarioNaTela()
 { 
     return userInScreen.innerText = user
 }
-
